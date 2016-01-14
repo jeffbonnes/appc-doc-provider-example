21941507a073322248651fefd123e2e6855e0bf2
Jeff Bonnes <jeffb@gameshape.com>
2016-01-14 13:44:25 +1100

    Initial Commit

---
+++ extension/My Cool App/MyCoolAppDocProvider/DocumentPickerViewController.h
@@ -0,0 +1,13 @@
+//
+//  DocumentPickerViewController.h
+//  MyCoolAppDocProvider
+//
+//  Created by Jeffrey Bonnes on 14/05/2015.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DocumentPickerViewController : UIDocumentPickerExtensionViewController
+
+@end
---
+++ extension/My Cool App/MyCoolAppDocProviderFileProvider/FileProvider.h
@@ -0,0 +1,13 @@
+//
+//  FileProvider.h
+//  MyCoolAppDocProviderFileProvider
+//
+//  Created by Jeffrey Bonnes on 14/05/2015.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FileProvider : NSFileProviderExtension
+
+@end
